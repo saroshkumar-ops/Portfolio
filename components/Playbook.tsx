e168486c44b95e949f87252c06204edc3d0bff28
Sarosh <saroshkumar00@gmail.com>
2026-07-25 10:55:55 +0530

    approach section image

--- components/Playbook.tsx
+++ components/Playbook.tsx
@@ -29,7 +29,7 @@ import ScrubVideoFrames, { type ScrubVideoFramesHandle } from "@/components/ui/s
 
 gsap.registerPlugin(ScrollTrigger);
 
-const APPROACH_FRAME_COUNT = 231;
+const APPROACH_FRAME_COUNT = 241;
 const approachFrameSrc = (index: number) => `/approach/img_${String(index).padStart(5, "0")}.jpg`;
 
 const TECH_LOGOS: LogoItem[] = [
